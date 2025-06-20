8926cf5f66951ca006c587ba94e5cbf7b8d634f8
mayara-janjao <mayara.janjao@escola.pr.gov.br>
2025-06-20 16:36:43 -0300

    .

---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criacartao(
+    "programaçâo",
+    "o que é python",
+    "o paython é uma linguage de programaçâo"
+)
+
+criacartao(
+    "programaçâo",
+    "o que é front-end",
+    "O front-end é a parte visual de um site ou sistima."
+)
+
+criacartao(
+    "programaçâo",
+    "o que é back-end",
+    "o back-end é a parte invisivil do sistema."
+)
+
+criacartao(
+    "musica",
+    "o que é mpb",
+    "mpb sigla para musica popular brasileira é um gênero musical."
+)
+
+criacartao(
+    "musica",
+    "o que é pop",
+    "é um gênero musical caracterizado por ser acessível ao público em geral."
+)
+
+criacartao(
+    "musica",
+    "o que é eletrônico",
+    "gênero musical modificado por equipamentos e instrumentos eletrônicos, como sintetizadores, computadores."
+)
